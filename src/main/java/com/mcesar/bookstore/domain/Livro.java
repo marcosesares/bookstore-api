14b4dc07aa3d3f552984402305e88db346bca670
marcosesares <marcosesares@gmail.com>
2021-07-14 15:38:36 -0300

    Adding Field validations

--- src/main/java/com/mcesar/bookstore/domain/Livro.java
+++ src/main/java/com/mcesar/bookstore/domain/Livro.java
@@ -9,6 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -20,8 +23,17 @@ public class Livro implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@NotEmpty(message = "Campo TITULO é requerido")
+	@Length(min = 3, max = 50, message = "O campo TITULO deve ter entre 3 e 50 caracteres")
 	private String titulo;
+
+	@NotEmpty(message = "Campo AUTOR é requerido")
+	@Length(min = 3, max = 50, message = "O campo AUTOR deve ter entre 3 e 50 caracteres")
 	private String autor;
+
+	@NotEmpty(message = "Campo TEXTO é requerido")
+	@Length(min = 10, message = "O campo TEXTO deve ter no mínimo 10 caracteres")
 	private String texto;
 
 	@JsonIgnore
--- src/main/java/com/mcesar/bookstore/resources/LivroResource.java
+++ src/main/java/com/mcesar/bookstore/resources/LivroResource.java
@@ -4,8 +4,11 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -22,6 +25,7 @@ import com.mcesar.bookstore.domain.Livro;
 import com.mcesar.bookstore.dtos.LivroDTO;
 import com.mcesar.bookstore.services.LivroService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/livros")
 public class LivroResource {
@@ -43,7 +47,7 @@ public class LivroResource {
 	}
 
 	@PostMapping
-	public ResponseEntity<Livro> create(@RequestParam(value = "categoria", defaultValue = "0") Long cat_id, @RequestBody Livro obj) {
+	public ResponseEntity<Livro> create(@RequestParam(value = "categoria", defaultValue = "0") Long cat_id, @Valid @RequestBody Livro obj) {
 		obj = service.create(cat_id, obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
@@ -51,14 +55,14 @@ public class LivroResource {
 
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Livro> update(@PathVariable Long id, @RequestParam(value = "categoria", defaultValue = "0") Long cat_id,
-			@RequestBody Livro obj) {
+			@Valid @RequestBody Livro obj) {
 		Livro newObj = service.update(id, cat_id, obj);
 		return ResponseEntity.ok().body(newObj);
 	}
 
 	@PatchMapping(value = "/{id}")
 	public ResponseEntity<Livro> updatePatch(@PathVariable Long id, @RequestParam(value = "categoria", defaultValue = "0") Long cat_id,
-			@RequestBody Livro obj) {
+			@Valid @RequestBody Livro obj) {
 		Livro newObj = service.update(id, cat_id, obj);
 		return ResponseEntity.ok().body(newObj);
 	}
