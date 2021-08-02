ca7306e001a7eef6488601442431ac3852f7056f
marcosesares <marcosesares@gmail.com>
2021-08-02 16:00:05 -0300

    teste

--- src/main/java/com/mcesar/bookstore/services/CategoriaService.java
+++ src/main/java/com/mcesar/bookstore/services/CategoriaService.java
@@ -38,7 +38,7 @@ public class CategoriaService {
 		obj.setDescricao(objDTO.getDescricao());
 		try {
 			return repository.save(obj);
-		} catch (IllegalArgumentException e) {
+		} catch (RuntimeException e) {
 			throw new com.mcesar.bookstore.exceptions.DataIntegrityViolationException("data integrity");
 		}
 	}
